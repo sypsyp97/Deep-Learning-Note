6afef7954ea3e9cddb5de290d5dd4327d1a7278e
sypsyp97 <sypsyp97@gmail.com>
2020-11-13 06:23:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from pattern import Checker, Circle, Spectrum
+
+ck = Checker(10, 25)
+ck.draw()
+ck.show()
+
+cc = Circle(1024, 200, (512, 256))
+cc.draw()
+cc.show()
+
+sp = Spectrum(256)
+sp.draw()
+sp.show()
